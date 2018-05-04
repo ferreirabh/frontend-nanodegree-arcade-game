a7748b51c4ceba421744a70913db8630eb08a3cd
Bruno Ferreira <bruno.ferreira@geoflorestas.com.br>
2018-05-04 08:46:12 -0300

    Atualizacao na classe Player e pre-implementacao da renderizacao dos enemies

--- js/app.js
+++ js/app.js
@@ -1,77 +1,79 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function () {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.x = 0;
-    this.y = 0;
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.x;
+  this.y;
+  this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+Enemy.prototype.update = function (dt) {
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+
+  setInterval(() => this.x += 101, dt);
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+Enemy.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function() {
-    this.sprite = 'images/char-boy.png';
+var Player = function () {
+  this.x;
+  this.y;
+  this.sprite = 'images/char-boy.png';
+};
 
-    this.x = 3 * 83;
+Player.prototype.update = function (dt) {
+  if (!this.x && !this.y) {
+    this.x = canvas.width / 2 - Resources.get(this.sprite).width / 2;
     this.y = 4 * 101;
+
+  }
 };
 
-Player.prototype.update = function(dt) {
-
+Player.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.render = function() {
-    const img = Resources.get(this.sprite);
-    this.x = canvas.width / 2 - img.width / 2;
-    
-    ctx.drawImage(img, this.x, this.y);
-};
+Player.prototype.handleInput = function (direction) {
+  switch (direction) {
+    case 'left':
+      this.x -= 101;
+      break;
 
-Player.prototype.handleInput = function(direction) {
-    switch (direction) {
-        case 'left':
-            this.x -= 101;
-            break;
+    case 'up':
+      this.y -= 83;
+      break;
 
-        case 'up':
-            this.y -= 83;
-            break;
+    case 'right':
+      this.x += 101;
+      break;
 
-        case 'right':
-            this.x += 101;
-            break;
+    case 'down':
+      this.y += 83;
+      break;
+  }
 
-        case 'down':
-            this.y += 83;
-            break;
-
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
 
 var allEnemies = [];
 
-for (let i = 0; i > 4; i++) {
-    allEnemies.push(new Enemy());
+for (let i = 0; i > 2; i++) {
+  allEnemies.push(new Enemy());
 }
 
 var player = new Player();
@@ -84,13 +86,13 @@ var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+document.addEventListener('keyup', function (e) {
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
