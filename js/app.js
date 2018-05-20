501a0e7050ba6dac8397824f7f133c9e45b43c7c
Bruno Henrique Ferreira <ferreirabrunodev@gmail.com>
2018-05-20 11:04:21 -0300

    Implementacao da colisao e contagem de pontos do player

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var Enemy = function (line) {
   // a helper we've provided to easily load images
   this.sprite = 'images/enemy-bug.png';
   this.x = Math.random() * (1000 - 0) + 0;
-  this.y = line * 62.5 + (line == 0 ? 62.5 : 82.5);
+  this.y = line ? 321 - line * 83 : 72;
 };
 
 // Update the enemy's position, required method for game
@@ -18,7 +18,11 @@ Enemy.prototype.update = function (dt) {
   // all computers.
   this.x = this.x > 600 ? -100 : this.x; 
 
-  setInterval(() => this.x += 1, 10000000 * dt);
+  setInterval(() => {
+    this.x += 1;
+
+    this._checkColision();
+  }, 10000000 * dt);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -26,25 +30,43 @@ Enemy.prototype.render = function () {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Enemy.prototype._checkColision = function() {
+  if (this.x == player.x && this.y == player.y) {
+    player.points = 0;
+    player.reset();
+  }
+}
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
 var Player = function () {
+  this._display = document.querySelector('.counter');
   this.x;
   this.y;
+  this.points = 0;
   this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.update = function (dt) {
-  if (!this.x && !this.y) {
-    this.x = canvas.width / 2 - Resources.get(this.sprite).width / 2;
-    this.y = 4 * 101;
-  }
+  if (!this.x && !this.y) 
+    this.reset();
+  
+  this.updatePointsDisplay();  
 };
 
 Player.prototype.render = function () {
-  if (this.y < 0) this.y = 404;
+  if (this.y < 0) {
+    this.points += 1;
+    this.y = 404;
+    console.log(this.points);
+    
+    
+  } else if (this.y > 404) {
+    this.y = 404;
+  }
+
 
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
@@ -71,14 +93,23 @@ Player.prototype.handleInput = function (direction) {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+Player.prototype.reset = function() {
+  this.x = canvas.width / 2 - Resources.get(this.sprite).width / 2;
+  this.y = 4 * 101;
+}
 
+Player.prototype.updatePointsDisplay = function() {
+  this._display.textContent = `${this.points} pontos`;
+}
+
+
+var player = new Player();
 var allEnemies = [];
 
 for (let i = 0; i < 3; i++) {
   allEnemies.push(new Enemy(i));
 }
 
-var player = new Player();
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
